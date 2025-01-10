642a70f749c4259c0bb7dda7c0e7644c2f2b9d83
Isaac Liu <IsaacLiu2009@outlook.com>
2025-01-10 21:32:21 +0800

    # new file:   .vscode/settings.json 	# new file:   mono1200 	# new file:   mono_1200_generator.cpp 	# new file:   output/NotoSansMonoCJKsc-Bold-Mono1200.ttf 	# new file:   output/NotoSansMonoCJKsc-Regular-Mono1200.ttf 	# new file:   src/NotoSansMonoCJKsc-Bold.otf 	# new file:   src/NotoSansMonoCJKsc-Regular.otf

---
+++ mono_1200_generator.cpp
@@ -0,0 +1,81 @@
+#include <cstdlib>
+#include <cstdint>
+
+#include <string>
+#include <format>
+#include <iostream>
+#include <sstream>
+
+constexpr uintmax_t hash(const std::string_view& str) {
+    uintmax_t hash = 0u;
+    for (const char ch: str) {
+        hash *= 131u;
+        hash += ch;
+    }
+    return hash;
+}   
+int system_command(const std::string& str) {
+    return std::system(str.c_str());
+}
+std::string read_command(std::istream& is) {
+    std::string command;
+    while (is.peek() == '\n') is.ignore();
+    std::getline(is, command);
+    return command;
+}
+void to_ttf(const std::string_view& str) {
+    std::cout << std::format("Transfering {0:s}.otf to {0:s}.ttf ...", str) << std::endl;
+    system_command(std::format("otf2ttf \"./src/{0:s}.otf\" -o \"./output/{0:s}.ttf\"", str));
+}
+void ttf_to_mono1200(const std::string_view& str) {
+    std::cout << std::format("Generating {0:s}-Mono 1200.ttf from {0:s}.ttf ...", str) << std::endl;
+    system_command(std::format("monoback mono1200 \"./output/{:s}.ttf\"", str));
+}
+void otf_to_mono1200(const std::string_view& str) {
+    to_ttf(str);
+    ttf_to_mono1200(str);
+    std::remove(std::format("./output/{:s}.ttf", str).c_str());
+}
+int main() {
+    std::cout << "Generator for CJK Fonts.\nRemember to install dependencies[otf2ttf, monoback] first." << std::endl;
+    std::string command_name;
+    while (true) {
+        std::cout << "Mono 1200 Generator:~$ ";
+        std::istringstream iss(read_command(std::cin));
+        iss >> command_name;
+        switch (hash(command_name)) {
+            case hash("install"): {
+                std::string package_name;
+                if (iss >> package_name) {
+                    if (package_name != "otf2ttf" && package_name != "monoback")
+                        std::cout << "Unknown package." << std::endl;
+                    else 
+                        system_command(std::format("sudo pip install {}", package_name));
+                    break;
+                }
+                system_command("sudo pip install otf2ttf");
+                system_command("sudo pip install monoback");
+                break;
+            }
+            case hash("auto_generate"): {
+                otf_to_mono1200("NotoSansMonoCJKsc-Regular");
+                otf_to_mono1200("NotoSansMonoCJKsc-Bold");
+                break;
+            }
+            case hash("generate"): {
+                std::string filename;
+                iss >> filename;
+                const auto ext = filename.rfind('.');
+                switch(hash(filename.substr(ext))) {
+                    case hash(".otf"): otf_to_mono1200(filename.substr(0, ext)); break;
+                    case hash(".ttf"): ttf_to_mono1200(filename.substr(0, ext)); break;
+                    default: std::cout << "Unsupported file format: " << filename.substr(ext) << std::endl;
+                }
+            }
+            case hash("exit"): return 0;
+            default:
+                std::cout << "Unknown command." << std::endl;
+        }
+    }
+    return 0;
+}
